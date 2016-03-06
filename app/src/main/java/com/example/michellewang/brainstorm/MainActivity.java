cddb87d443fc8de4597e4cada1005b04efc4587c
Frederick Kennedy <frederick.k0110@icloud.com>
2016-03-05 18:17:38 -0800

    Implemented Login UI & Logout Activity

--- app/src/main/java/com/example/michellewang/brainstorm/MainActivity.java
+++ app/src/main/java/com/example/michellewang/brainstorm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.michellewang.brainstorm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -9,20 +10,25 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.firebase.client.Firebase;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    Firebase ref = new Firebase("http://csm117-brainstorm.firebaseio.com");
     NavigationView navigationView = null;
     Toolbar toolbar = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
 
         // Initial Fragment to display
         MainActivityFragment fragment = new MainActivityFragment();
@@ -77,8 +83,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_logout) {
+            ref.unauth();
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+
         }
 
         return super.onOptionsItemSelected(item);
