1c24a58125cdea516d42ed7e7539338d4982484b
Michelle Wang <michellewang13@ucla.edu>
2016-03-06 15:07:34 -0800

    added new login stuff

--- app/src/main/java/com/example/michellewang/brainstorm/MainActivity.java
+++ app/src/main/java/com/example/michellewang/brainstorm/MainActivity.java
@@ -14,8 +14,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Firebase ref = new Firebase("https://csm117-brainstorm.firebaseio.com/");
 
     NavigationView navigationView = null;
     Toolbar toolbar = null;
@@ -79,6 +83,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_logout) {
+            ref.unauth();
             Intent logout = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(logout);
         }
